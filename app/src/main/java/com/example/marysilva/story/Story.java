a46429b225cfdc655aed939de04a3388adcb813d
marysilva <msilva2@mail.sfsu.edu>
2016-11-28 13:08:33 -0800

    Added SingleFragmentActivity, StoryActivity, etc

--- app/src/main/java/com/example/marysilva/story/Story.java
+++ app/src/main/java/com/example/marysilva/story/Story.java
@@ -1,13 +1,40 @@
 package com.example.marysilva.story;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import java.util.UUID;
 
-public class Story extends AppCompatActivity {
+/**
+ * Created by marysilva on 11/25/16.
+ */
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_story);
+public class Story {
+    private UUID mUUID;
+    private String mName;
+    private String mDescription;
+    //TODO: A picture field ImageView should work - Worry about this later
+
+    public Story(){
+        mUUID = UUID.randomUUID();
+        //TODO: add a description to this method - Tony? In the crime
+        // app example this was the Date field.
+    }
+
+    public UUID getId() {
+        return mUUID;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public void setDescription(String description) {
+        mDescription = description;
     }
 }

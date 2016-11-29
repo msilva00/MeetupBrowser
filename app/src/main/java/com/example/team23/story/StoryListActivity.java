21c525300544c2f9606330b55391c0aa16c22f72
jkmann <jkmann@mail.sfsu.edu>
2016-11-29 14:29:58 -0800

    Updated StoryListActivity and StoryListFragment. and added list_item_story.xml

--- app/src/main/java/com/example/team23/story/StoryListActivity.java
+++ app/src/main/java/com/example/team23/story/StoryListActivity.java
@@ -1,15 +1,13 @@
-//package com.example.marysilva.story;
-//TODO: Uncomment this once RecyclerView (StoryListFragment) is updated
-//import android.support.v4.app.Fragment;
-//
-///**
-// * Created by marysilva on 11/28/16.
-// */
-//
-//public class StoryListActivity extends SingleFragmentActivity{
-//
-//    @Override
-//    protected Fragment createFragment() {
-//        return new StoryListFragment();
-//    }
-//}
+package com.example.team23.story;
+import android.support.v4.app.Fragment;
+
+
+
+
+public class StoryListActivity extends SingleFragmentActivity{
+
+    @Override
+    protected Fragment createFragment() {
+        return new StoryListFragment();
+    }
+}
--- app/src/main/java/com/example/team23/story/StoryListFragment.java
+++ app/src/main/java/com/example/team23/story/StoryListFragment.java
@@ -1,27 +1,112 @@
-//TODO: Jasmine you can delete StoryListFragment and StoryListActivity and start from scratch
-//this was just to breakdown what parts we have left to do
+package com.example.team23.story;
 
-//package com.example.marysilva.story;
-//
-//import android.support.v4.app.Fragment;
-//import android.support.v7.widget.RecyclerView;
-//
-//
-//public class StoryListFragment extends Fragment {
-//    //TODO: Set Up RecyclerView (p.181-182) fragment_story_list.xml already created - Jasmine?
-//    public class StoryAdapter extends RecyclerView.ViewHolder {
-//
-//    }
-//
-//    //TODO: Set up view holder (p188)
-//    private class StoryHolder extends RecyclerView.ViewHolder{
-//        /* Instead of toast (p 191) new activity for details of story - worry about this last?
-//            Instead of CheckBox(p 188) ImageView - also worry about this last   */
-//    }
-//
-//
-//    //TODO: StoryAdapter (p. 182-183) - Jasmine?
-//    private class StoryAdapter extends RecyclerView.Adapter<StoryHolder>{
-//
-//    }
-//}
+
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView.Adapter;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.ImageView;
+
+import java.util.Collections;
+import java.util.List;
+
+
+public class StoryListFragment extends Fragment {
+
+    private RecyclerView mStoryRecyclerView;
+    private StoryAdapter mAdapter;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_story_list, container, false);
+
+        mStoryRecyclerView = (RecyclerView) view
+                .findViewById(R.id.story_recycler_view);
+        mStoryRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        updateUI();
+        return view;
+    }
+
+    private void updateUI(){
+        StorySingleton storySingleton = StorySingleton.get(getActivity());
+        List<Story> stories = storySingleton.getStories();
+
+        mAdapter = new StoryAdapter(stories);
+        mStoryRecyclerView.setAdapter(mAdapter);
+    }
+
+    public class StoryAdapter extends RecyclerView.Adapter<StoryHolder> {
+
+
+        private List<Story> mStories;
+
+        public StoryAdapter(List<Story> stories) {
+            mStories = stories;
+        }
+
+        @Override
+        public StoryHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+            View view = layoutInflater.inflate(R.layout.list_item_story, parent, false);
+            return new StoryHolder(view);
+        }
+        @Override
+        public void onBindViewHolder(StoryHolder holder, int position) {
+            Story story = mStories.get(position);
+            holder.bindStory(story);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mStories.size();
+        }
+  }
+
+
+
+
+  private class StoryHolder extends RecyclerView.ViewHolder {
+
+      private TextView mTitleTextView;
+      private TextView mDescription;
+      private ImageView imageView;
+
+      private Story mStory;
+
+      public StoryHolder(View itemView) {
+          super(itemView);
+          //itemView.setOnClickListener();
+
+          mTitleTextView = (TextView) itemView.findViewById(R.id.list_item_story_title_text_view);
+          mDescription = (TextView) itemView.findViewById(R.id.description);
+          imageView = (ImageView) itemView.findViewById(R.id.imageView);
+      }
+
+
+      public void bindStory(Story story) {
+          mStory = story;
+          mTitleTextView.setText(mStory.getName());
+          mDescription.setText(mStory.getDescription());
+          //imageView.setImageResource(list.get(position).imageId);
+      }
+/*
+      @Override
+      public void onClick(View v) {
+          Intent intent = new Intent(getActivity(), StoryAcivity.class);
+          startActivity(intent);
+      }*/
+
+
+  }
+}

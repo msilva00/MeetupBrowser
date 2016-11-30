ba7d6ba9b07589c4f34290275353815b77ba5658
jkmann <jkmann@mail.sfsu.edu>
2016-11-30 14:35:42 -0800

    Updated Story activity with intent and made some changes to StoryFragment

--- app/src/main/java/com/example/team23/story/StoryAcivity.java
+++ app/src/main/java/com/example/team23/story/StoryAcivity.java
@@ -1,12 +1,26 @@
 package com.example.team23.story;
 
 import android.support.v4.app.Fragment;
+import android.content.Context;
+import android.content.Intent;
+
+import java.util.UUID;
 
 public class StoryAcivity extends SingleFragmentActivity {
 
+    private static final String EXTRA_STORY_ID =
+            "com.example.team23.story.story_id";
+
+    public static Intent newIntent(Context packageContext, UUID storyId) {
+        Intent intent = new Intent(packageContext, StoryAcivity.class);
+        intent.putExtra(EXTRA_STORY_ID, storyId);
+        return intent;
+    }
     @Override
     protected Fragment createFragment() {
-        return new StoryFragment();
+        UUID storyId = (UUID) getIntent()
+                .getSerializableExtra(EXTRA_STORY_ID);
+        return StoryFragment.newInstance(storyId);
     }
 
 }
--- app/src/main/java/com/example/team23/story/StoryFragment.java
+++ app/src/main/java/com/example/team23/story/StoryFragment.java
@@ -9,21 +9,33 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import java.util.UUID;
+
 
 import com.example.team23.story.R;
 
-/**
- * Created by marysilva on 11/25/16.
- */
+
 
 public class StoryFragment extends Fragment{
     private Story mStory;
     private EditText mTitleField;
 
+    private static final String ARG_STORY_ID ="story_id";
+
+    public static StoryFragment newInstance(UUID storyId) {
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_STORY_ID, storyId);
+
+        StoryFragment fragment = new StoryFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        mStory = new Story();
+        UUID storyId = (UUID) getArguments().getSerializable(ARG_STORY_ID);
+        mStory = StorySingleton.get(getActivity()).getStory(storyId);
     }
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){

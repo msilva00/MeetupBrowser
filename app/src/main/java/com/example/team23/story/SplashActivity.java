63aec48ef8f60d8641b60f2eb56151e640b06425
jkmann <jkmann@mail.sfsu.edu>
2016-11-30 22:30:10 -0800

    purple splash screen

--- app/src/main/java/com/example/team23/story/SplashActivity.java
+++ app/src/main/java/com/example/team23/story/SplashActivity.java
@@ -4,15 +4,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity{
 
+    private final int SPLASH_DISPLAY_LENGTH = 6000;
+
+    /** Called when the activity is first created. */
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent intent = new Intent(this, StoryAcivity.class);
-        startActivity(intent);
-        finish();
-    }
-}
+
+                Intent intent = new Intent(this,StoryListFragment.class);
+                startActivity(intent);
+                finish();
+            }
+        }
+
+
